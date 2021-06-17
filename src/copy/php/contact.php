dfae65777a76a4049f545f3dbfb4100b812da4dc
Andrew Sazonov <AndrewSazonov@users.noreply.github.com>
2021-06-17 10:30:13 +0200

    Set default email address to contact.php

--- src/copy/php/contact.php
+++ src/copy/php/contact.php
@@ -18,8 +18,7 @@
         }
 
         // Set the recipient email address.
-        // FIXME: Update this to your desired email address.
-        $recipient = "support@easydiffraction.org";
+        $recipient = "contact@easyscience.software";
 
         // Set the email subject.
         $subject = "New contact from $name";
